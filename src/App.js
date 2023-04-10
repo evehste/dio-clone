eb6cdd26a7fe1af8bb193a616f24095ba3b30ea4
Evelin  Stefani <evelin-stefani@hotmail.com>
2023-04-10 17:17:31 -0300

    Funcionalidade Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Feed } from "./pages/Feed/Feed";
+import { Home } from './pages/Home/Home';
+import { Login } from './pages/Login/Login';
+import { GlobalStyle } from './styles/global';
+
 const App = () => {
   return (
-    <div>
-     olá
-    </div>
+    <Router>
+     <GlobalStyle />
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/feed" element={<Feed />} />
+     </Routes>
+    </Router>
   );
 }
 
